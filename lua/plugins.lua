0e6ef8b15ef3346b2bab9c18190bde52d2fdb200
Ivan <ordinarydev@protonmail.com>
2023-09-29 11:51:10 +0500

    Migrate to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,55 +1,57 @@
-local status, packer = pcall(require, "packer")
-if (not status) then return end
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-packer.startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+require("lazy").setup({
+  -- Git
+  { 'lewis6991/gitsigns.nvim', tag = 'v0.6' },
+  -- Lualine
+  { 'nvim-lualine/lualine.nvim' },
+  -- Gruvbox theme
+  { 'ellisonleao/gruvbox.nvim' },
 
-    -- Git
-    use {
-        'lewis6991/gitsigns.nvim',
-        tag = 'v0.6',
-    }
+  -- Telescope
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
 
-    -- Lualine
-    use 'nvim-lualine/lualine.nvim'
-    
-    -- Gruvbox theme
-    use 'ellisonleao/gruvbox.nvim'
+  -- Nvim-treesitter
+  {
+    'nvim-treesitter/nvim-treesitter',
+    tag = 'v0.9.0',
+    init = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  },
 
-    -- Telescope
-    use {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.2',
-        requires = {
-            'nvim-lua/plenary.nvim',
-        },
-    }
+  -- LSP
+  { 'neovim/nvim-lspconfig' },
+  {
+    'ray-x/lsp_signature.nvim',
+  },
 
-    -- Nvim-treesitter
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        tag = 'v0.9.0',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
-
-    -- LSP
-    use 'neovim/nvim-lspconfig'
-    use {
-        "ray-x/lsp_signature.nvim",
-    }
-
-    -- Neo-tree
-    use {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-        }
-    }
-end)
+  -- Neo-tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+  },
+})
